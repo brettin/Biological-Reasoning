7cc0d0ce27e66920596c1e07a24cfd21a3323c3a
brettin <brettin@anl.gov>
2025-04-29 22:03:35 -0500

    new template

---
+++ env.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=$PYTHONPATH:bio_reasoning
+#
+# Edit these to match your local environment
+#
+export MODEL_BASE_URL="model api endpoint url"
+export MODEL_API_KEY="api key for model api endpoint"
+export MODEL_NAME="served model api endpoint url"
