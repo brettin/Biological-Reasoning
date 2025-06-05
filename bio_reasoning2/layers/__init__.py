6601daf5fad300e30e9892df8f791b7f741e32eb
Peng Ding <pding.dp@foxmail.com>
2025-06-05 04:55:47 -0500

    ♻️ refactor(layers): update layer B tool import and export
    - replace VisualDescriber import with visual_describer_factory
    - update __all__ to reflect the renamed export

--- bio_reasoning2/layers/__init__.py
+++ bio_reasoning2/layers/__init__.py
@@ -1,11 +1,11 @@
 from .a import ParametricMemory
-from .b import VisualDescriber
+from .b import visual_describer_factory
 
 __all__ = [
     # layer A tools
     "ParametricMemory",
     # layer B tools
-    "VisualDescriber",
+    "visual_describer_factory",
     # layer C tools
     ...,
 ]
