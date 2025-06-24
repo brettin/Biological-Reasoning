44ea52d9799c2e05c97ed5ebebd66cc104332126
Peng Ding <pding.dp@foxmail.com>
2025-06-24 17:18:58 -0500

    ♻️ refactor(reasoning_modes): streamline parametric memory registration
    - replace ParametricMemory with parametric_memory_factory for improved modularity
    - simplify layer A registration process by directly using parametric_memory factory

--- bio_reasoning/reasoning_modes.py
+++ bio_reasoning/reasoning_modes.py
@@ -1,11 +1,10 @@
 import os
 
-from cicada.core import MultiModalModel
 from dotenv import load_dotenv
 from toolregistry import ToolRegistry
 from toolregistry.hub import WebSearchGoogle
 
-from .layers.a.parametric_memory import ParametricMemory
+from .layers.a.parametric_memory import parametric_memory_factory
 from .layers.b import visual_describer_factory
 
 
@@ -38,18 +37,14 @@ class ExampleReasoningMode(ReasoningMode):
         layer_c = ToolRegistry(name="Layer C")
 
         load_dotenv()
-        # register layer A tools
-        model = MultiModalModel(
+
+        parametric_memory = parametric_memory_factory(
             api_key=os.getenv("API_KEY"),
             api_base_url=os.getenv("BASE_URL"),
             model_name=os.getenv("MODEL_NAME"),
+            system_prompt="You are an expert in biology. You are given a question and you need to answer it with the best of your knowledge.",
         )
-        layer_a.register_from_class(
-            ParametricMemory(
-                model=model,
-                system_prompt="You are an expert in biology. You are given a question and you need to answer it with the best of your knowledge.",
-            )
-        )
+        layer_a.register(parametric_memory)
         # register layer B tools
         system_prompt = "You are professional biologist with specialty in image analysis. Please describe the image in detail."
 
