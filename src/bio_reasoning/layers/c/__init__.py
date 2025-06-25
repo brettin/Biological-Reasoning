c174ad76650e26584daa787126ddc6bb0425191c
Peng Ding <pding.dp@foxmail.com>
2025-06-25 04:55:34 -0500

    🔧 chore(layers): restructure layer c file organization
    - rename layer c file to __init__.py to facilitate package structure
    - remove placeholder comment indicating future implementation

--- src/bio_reasoning/layers/c/__init__.py
+++ src/bio_reasoning/layers/c/__init__.py
@@ -1,5 +1,3 @@
 """
 Layer C encompasses external knowledge sources - APIs, databases, and knowledge graphs - to provide access to large, dynamic, or regulated datasets that cannot reside fully within models.
 """
-
-# to be implemented
